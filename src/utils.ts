178237d1d583bdf6a099e1ce73ef7c2357df4ef9
Soos3D <99700157+soos3d@users.noreply.github.com>
2022-12-27 16:54:51 -0300

    optimize test

--- src/utils.ts
+++ src/utils.ts
@@ -31,6 +31,58 @@ export const hitEndpointWithResponseTime = async (
   provider: ethers.providers.Provider,
   endpoint: string
 ): Promise<EndpointWithTimeResponse> => {
+
+  // Change the rounds constant to run the test more or less times
+  const rounds = 20;
+  let time = 0;
+  let responses = []
+
+
+  for (let round = 0; round < rounds; round++) {
+    const start = window.performance.now();
+    // @ts-expect-error
+    await provider[endpoint]!();
+    const end = window.performance.now();
+
+    const result = end - start;
+    console.log(`Request number ${round}: ${result}`)
+
+    // Add each response time to the array
+    responses.push(result)
+
+    // Sum the total time so far
+    time += result;
+    //console.log(time)
+  }
+
+  // Isolate the first call of the sequence
+  const firstCall = responses[0]
+
+  // Calculate the average removing the first call
+  const average = Math.round((time - firstCall) / (rounds -1));
+    return {
+      responseTime: average,
+    };
+
+};
+
+export const getBlockNumberWithResponseTime = async (
+  provider: ethers.providers.Provider
+) => {
+  const start = window.performance.now();
+  const blockNumber = await provider.getBlockNumber();
+  const end = window.performance.now();
+  return {
+    blockNumber,
+    responseTime: Math.round(end - start),
+  };
+};
+
+/*
+export const hitEndpointWithResponseTime = async (
+  provider: ethers.providers.Provider,
+  endpoint: string
+): Promise<EndpointWithTimeResponse> => {
   const start = window.performance.now();
   // @ts-expect-error
   const blockNumber = await provider[endpoint]!();
@@ -52,3 +104,6 @@ export const getBlockNumberWithResponseTime = async (
     responseTime: Math.round(end - start),
   };
 };
+
+
+ */
